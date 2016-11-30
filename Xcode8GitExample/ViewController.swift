34b9286624c1cccff5f66814ff152a765c97928f
Michael Rüttger <mruettger@michaels-imac.fritz.box>
2016-11-30 12:53:19 +0100

    Initial Commit

---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by Michael Rüttger on 30.11.16.
+//  Copyright © 2016 Rüttger Mediendesign. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
